21a8ced9b70c99c8ea3aabd6811d64bc99bd2133
DanMihneaVlad <mihnea.dan@outlook.com>
2021-04-23 20:56:31 +0300

    TC-2 #time 2h #comment Added the appointment class and an arraylist of appointments to the doctor class, finishing the task ahead of schedule #resolve

--- src/main/java/org/fis2021/terpsicare/model/Doctor.java
+++ src/main/java/org/fis2021/terpsicare/model/Doctor.java
@@ -1,10 +1,13 @@
 package org.fis2021.terpsicare.model;
 
+import java.util.ArrayList;
+
 public class Doctor extends User {
 
     private String name;
     private String medicalSpecialty;
     private String phoneNumber;
+    private ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 
     public Doctor(String username, String password, String name, String medicalSpecialty, String phoneNumber) {
         super(username, password, "Doctor");
